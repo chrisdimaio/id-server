587d99965d6af8ea65ec6fdd19e28738c0f95e9a
Chris DiMaio <chrisdimaio@gmail.com>
2020-10-22 17:51:04 -0400

    Using mac as part of worker id

--- src/main/java/io/chrisdima/idserver/ID.java
+++ src/main/java/io/chrisdima/idserver/ID.java
@@ -1,19 +1,21 @@
 package io.chrisdima.idserver;
 
 import java.nio.charset.IllegalCharsetNameException;
+import java.util.Arrays;
 
 public class ID {
   private final int workerID;
   private int sequence;
 
-  public ID(int workerID){
-    this.workerID = get10BitWorkerID(workerID);
+  public ID(int machineID, int workerID){
+    // Combining the worker id and machine id to make the worker id a little more unique.
+    this.workerID = getWorkerID(Arrays.hashCode(new int[] {machineID, workerID}));
     this.sequence = -1;
   }
 
   public String generate() {
     return String.valueOf(
-        get41BitTimestamp() << 22 | this.workerID << 12 | getSequence());
+        getTimestamp() << 22 | this.workerID << 12 | getSequence());
   }
 
   private int getSequence() {
@@ -23,11 +25,11 @@ public class ID {
     return this.sequence;
   }
 
-  private int get10BitWorkerID(int workerID) {
+  private int getWorkerID(int workerID) {
     return workerID & 0x000003FF;
   }
 
-  private long get41BitTimestamp() {
+  private long getTimestamp() {
     return System.currentTimeMillis() & 0x00001FFFFFFFFFFL;
   }
 }
--- src/main/java/io/chrisdima/idserver/IdServer.java
+++ src/main/java/io/chrisdima/idserver/IdServer.java
@@ -1,23 +1,51 @@
 package io.chrisdima.idserver;
 
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class IdServer {
   private static HashMap<String, Integer> test = new HashMap<>();
   public static void main(String[] args) throws Exception {
-    ID id0 = new ID(0);
-    ID id1 = new ID(1);
-    ID id2 = new ID(2);
-    ID id3 = new ID(3);
+    ID id = new ID(getMachineId(), 0);
+    ID id1 = new ID(getMachineId(), 1);
 
+    String i0;
+    String i1;
+    long counter = 0;
     while(true) {
-      test(id0.generate());
-      test(id1.generate());
-      test(id2.generate());
-      test(id3.generate());
-      System.out.println("IDs in map: " + test.size());
+      i0 = id.generate();
+      i1 = id1.generate();
+      if(i0.equals(i1)) {
+        System.out.println("Collision between: " + i0 + " and " + i1);
+        System.exit(0);
+      }
+      if(counter++ % 2000000 == 0){
+        System.out.print(".");
+      }
+      if(counter % 100000000 == 0){
+        System.out.print(counter / 1000000 + " mil");
+        System.out.println();
+      }
     }
+  }
+
+  /**
+   * Machine ID is a hash of the MAC address of the first NIC with a MAC.
+   * @return int representing the machine's id, -1 if no
+   * @throws SocketException
+   */
+  private static int getMachineId() throws SocketException {
+    NetworkInterface networkInterface;
+    while(NetworkInterface.getNetworkInterfaces().hasMoreElements()) {
+      networkInterface = NetworkInterface.getNetworkInterfaces().nextElement();
+      if(networkInterface.getHardwareAddress() != null) {
+        return Arrays.hashCode(networkInterface.getHardwareAddress());
+      }
+    }
+    throw new IllegalStateException("System doesn't have a network interface with a MAC.");
   }
 
   static void test(String id) throws Exception {
